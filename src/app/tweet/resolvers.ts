200ae317c917281f52c0934e88d269f9d54cd4ad
Hemant <davidbroddy1@gmail.com>
2024-11-06 15:56:41 +0530

    "Added: upload image funtionality"

--- src/app/tweet/resolvers.ts
+++ src/app/tweet/resolvers.ts
@@ -1,14 +1,39 @@
 import { Tweet } from "@prisma/client";
 import { prismaClient } from "../../clients/db";
 import { graphqlContext } from "../../interfaces";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 export interface CreateTweetPayload {
     content: string;
     imageURL?: string;
   }
 
+const s3Client = new S3Client({
+  region: 'ap-south-1',
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_S3_ACCESS_KEY_SECRET || "",
+  }
+})
+
 const queries = {
-  getAllTweets : async() => await prismaClient.tweet.findMany({orderBy: {createdAt: "desc"}})
+  getAllTweets : async() => await prismaClient.tweet.findMany({orderBy: {createdAt: "desc"}}),
+
+  getSignedURLForTweet: async(parent: any, {imageName, imageType}: {imageName: string, imageType: string}, ctx: graphqlContext) => {
+    if(!ctx.user || !ctx.user.id) throw new Error("Unauthenticated");
+    
+    const allowedImageTypes = ["image/jpg", "image/jpeg", "image/png", "image/webp"];
+    if(!allowedImageTypes.includes(imageType)) throw new Error("Invalid image type");
+
+    const putObjectCommand  = new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET,
+      Key: `uploads/${ctx.user.id}/tweets/${imageName}-${Date.now()}.${imageType}`,
+    });
+
+    const signedURL = await getSignedUrl(s3Client, putObjectCommand);
+    return signedURL;
+  }
 }
 
 const mutations = {
--- src/app/tweet/types.ts
+++ src/app/tweet/types.ts
@@ -1,14 +1,15 @@
 export const types = `#graphql
-
     input CreateTweetData {
         content: String!
         imageURL: String
     }
 
+    scalar Date
     type Tweet {
         id: ID!
         content: String!
         imageURL: String
+        createdAt: Date
 
         author: User
     }
